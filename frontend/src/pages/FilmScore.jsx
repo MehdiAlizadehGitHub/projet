8e53e2ad0b3311c7c913480c10b0ee6af82297ae
Serine <bettayeb@et.esiea.fr>
2024-12-13 17:37:08 +0100

    update with other design and changes

--- frontend/src/pages/FilmScore.jsx
+++ frontend/src/pages/FilmScore.jsx
@@ -4,48 +4,55 @@ import Axios from "axios";
 import Card from "./components/cardscore";
 import '../App.css';
 import './css/Film.css';
+import logo from '../images/logo/1.svg';
 
 function FilmScore() {
-	const baseUrl = "/api";
-	const navigate = useNavigate();
+    const baseUrl = "/api";
+    const navigate = useNavigate();
 
-	const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
+    const handleBackToMenu = () => {
+        navigate('/');
+    };
 
-	const handleBackToMenu = () => {
-		navigate('/');
-	};
+    useEffect(() => {
+        Axios.get(`${baseUrl}/movies`).then((response) => {
+            setMovies(response.data);
+        });
+    }, []); // Ajout de dépendances pour éviter une boucle infinie
 
-	useEffect(() => {
-		Axios.get(`${baseUrl}/movies`)
-			.then((response) => {
-				setMovies(response.data);
-			});
-	});
-
-	return (
-		<div className="App">
-			<div className="container">
-				<div className="header">
-					<h1 className="pageTitle">Modify Your List</h1>
-					<button className="backButton" onClick={handleBackToMenu}>Back to Menu</button>
+    return (
+        <div className="App">
+            {/* Menu de navigation */}
+            <div className="navbar">
+				<div className="navbar-logo" onClick={handleBackToMenu}>
+					<img src={logo} alt="Film List Logo" />
 				</div>
-				<div className="cards">
-					{movies.map((movie) => (
-						<Card
-							key={movie.idmovies}
-							id={movie.idmovies}
-							title={movie.title}
-							rating={movie.rating}
-							genre={movie.genre}
-							className="card"
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+                <div className="navbar-item">
+                    <h2 onClick={() => navigate('/film-list')}>Film List</h2>
+                    <h2 onClick={() => navigate('/film-modify')}>Add/Edit</h2>
+                    <h2 onClick={() => navigate('/film-score')}>Score</h2>
+                </div>
+            </div>
+
+            {/* Contenu principal */}
+            <div className="container">
+                <div className="cards">
+                    {movies.map((movie) => (
+                        <Card
+                            key={movie.idmovies}
+                            id={movie.idmovies}
+                            title={movie.title}
+                            rating={movie.rating}
+                            genre={movie.genre}
+                            className="card"
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
-
 
 export default FilmScore;
